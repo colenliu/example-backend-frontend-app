caa51f7c944d58685d2398e2de11639b20496171
colenliu <liu.colen@hotmail.com>
2021-05-12 18:12:59 -0700

    added some POST and GET requests

--- api/app.js
+++ api/app.js
@@ -23,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
+app.use("/signup", indexRouter);
 app.use("/users", usersRouter);
 app.use("/testAPI", testAPIRouter);
 
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.post("/signup", async (req, res) => {
+  const { email, firstName } = req.body;
+
+  if (email && firstName) {
+    console.log("user recognized!");
+    return res.sendStatus(200);
+  }
 });
 
 module.exports = router;
